6b5596d353e4ab630133199eccdc6594d1f9ef2a
八谷剛史 <hacchy@hachiyatakeshinoMacBook-puro.local>
2021-03-16 13:42:31 +0900

    FEAT: implement download-test-data.sh script

---
+++ download-references/download-test-data.sh
@@ -0,0 +1,8 @@
+#!/bin/sh
+
+# Download test data
+wget https://www.dropbox.com/s/pykllpjm8i7l1y0/ERR1025659.small_1.fastq.gz
+wget https://www.dropbox.com/s/td72ys4iydhvq2h/ERR1025659.small_2.fastq.gz
+wget https://www.dropbox.com/s/vjq6wqwdtcipju6/ERR1419195.small_1.fastq.gz
+wget https://www.dropbox.com/s/gpcntc96m491o0l/ERR1419195.small_2.fastq.gz
+
